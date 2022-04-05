bb8d5855bd2a87993caaae436893cfb501c9dd66
Peter Lockett <cerwym@googlemail.com>
2022-04-05 16:33:27 +0000

    feat: Added express and dotenv-defaults

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express, { Express, Request, Response } from 'express';
+import "dotenv-defaults/config"
+
+const app: Express = express();
+const port = process.env.WS_PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`[main]: Server is running ::${port} 🚀`)
+})
